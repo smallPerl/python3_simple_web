7e6e18398e0409ccc3e54a671b411bf882c00986
heiguoer <2678243084@qq.com>
2016-12-19 17:02:45 +0800

    更新orm以及model错误
    更新orm以及model错误， orm的execut（）增加关闭操作

--- test.py
+++ test.py
@@ -1,21 +1,21 @@
-import socket
- 
-HOST, PORT = '192.168.182.134', 8080
- 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print ('Serving HTTP on port %s ...' % PORT)
-while True:
-    client_connection, client_address = listen_socket.accept()
-    request = client_connection.recv(1024)
-    print (request)
- 
-    http_response = """
-HTTP/1.1 200 OK
- 
-Hello, World!
-"""
-    client_connection.sendall(http_response)
-    client_connection.close()
+#! /usr/local/python3
+#! -*- conding:utf-8 _*_
+
+__author__ = 'z'
+
+#测试插入数据
+
+import orm
+import asyncio
+from models import User, Blog, Comment
+
+@asyncio.coroutine
+def test():
+	yield from orm.create_pool(loop, user='wwwdata', password='wwwdata', database='awesome', host='192.168.182.132')
+	u = User(name='Test', email='test5@example.com', passwd='123', image='about:blank'
+			)	
+	yield from u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test())
+loop.close()
